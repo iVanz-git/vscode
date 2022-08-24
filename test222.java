8a331476bb3f4275545154c27bb5fc58e6304b4f
iVanz-git <188808700@qq.com>
2022-08-24 11:32:41 +0800

    add something

--- test222.java
+++ test222.java
@@ -1,10 +1,15 @@
-public class test222{
-  public static void main(String[] args){
+public class test222 {
+  public static void main(String[] args) {
     int a = 1;
     int b = 2;
     int c = a + b;
     System.out.println(c);
     System.out.println(a);
     System.out.println(b);
+
+    System.out.println("1111111111111111111111");
+    System.out.println("222222222222222222");
+    System.out.println("333333333333");
+
   }
 }
